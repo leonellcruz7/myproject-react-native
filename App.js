e2515ebd700476475747c1016b571beb09893c63
Leonell Cruz <102971421+leonellcruz7@users.noreply.github.com>
2023-08-25 07:22:04 +0800

    reset

--- App.js
+++ App.js
@@ -1,35 +1,5 @@
 import Home from "./screens/Home";
-import * as Font from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect, useState } from "react";
-
-SplashScreen.preventAutoHideAsync();
-
-const loadFonts = () => {
-  return Font.loadAsync({
-    "poppins-regular": require("./assets/fonts/Poppins-Regular.ttf"),
-    "poppins-bold": require("./assets/fonts/Poppins-Bold.ttf"),
-  });
-};
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    const prepare = async () => {
-      try {
-        await loadFonts();
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setIsReady(true);
-        SplashScreen.hideAsync();
-      }
-    };
-    prepare();
-  }, []);
-  if (!isReady) {
-    return null;
-  }
   return <Home />;
 }
