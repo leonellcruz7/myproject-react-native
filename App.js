28756b10fba5e181edb3eb9ebdb20927c942a529
Leonell Cruz <102971421+leonellcruz7@users.noreply.github.com>
2023-08-25 19:39:42 +0800

    reset

--- App.js
+++ App.js
@@ -1,54 +1,9 @@
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./screens/Home";
 import About from "./screens/About";
 import ReviewDetails from "./screens/ReviewDetails";
-import * as Font from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect, useState } from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-SplashScreen.preventAutoHideAsync();
-
-const loadFont = () => {
-  return Font.loadAsync({
-    "poppins-regular": require("./assets/fonts/Poppins-Regular.ttf"),
-    "poppins-bold": require("./assets/fonts/Poppins-Bold.ttf"),
-  });
-};
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    const prepare = async () => {
-      try {
-        await loadFont();
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setIsReady(true);
-        SplashScreen.hideAsync();
-      }
-    };
-    prepare();
-  }, []);
-  if (!isReady) {
-    return null;
-  }
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "yellow",
-          },
-        }}
-      >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="ReviewDetails" component={ReviewDetails} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Home />;
 }
