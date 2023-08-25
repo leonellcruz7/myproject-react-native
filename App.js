758f9c2977815ce3042c867886d65c9cdcb09c59
Leonell Cruz <102971421+leonellcruz7@users.noreply.github.com>
2023-08-25 19:59:55 +0800

    reset

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
 import Home from "./screens/Home";
 import About from "./screens/About";
 import ReviewDetails from "./screens/ReviewDetails";
